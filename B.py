9e466a5d7ca1ec7024b7b1d8e9a3ae2ddd22cf9e
PolinaPonomar <ponomareva.polia2013@yandex.ru>
2018-11-19 17:44:24 +0300

    попытка 6

--- B.py
+++ B.py
@@ -1,7 +1,11 @@
-import dir2.dir1.A
+from dir1 import *
+#import dir2.dir1.A
 #from dir2.dir1 import A
 
-print(dir2.dir1.A.foo())
-dir2.dir1.A.x = 7
-print(dir2.dir1.A.foo())
+#print(dir2.dir1.A.foo())
+#dir2.dir1.A.x = 7
+#print(dir2.dir1.A.foo())
+dir1.A.x = 30
+print(dir1.A.x)
+#dir1.C.gen(4)
 
